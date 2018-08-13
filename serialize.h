3d45487b27dce1fc5a4c178083c01b422161543a
giacomo <nazzarogiacomo@gmail.com>
2018-08-13 13:30:29 +0200

    better test and comments

--- serialize.h
+++ serialize.h
@@ -1,10 +1,13 @@
 #include <cassert>
 #include <vector>
 #include <string>
-
+#include <functional>
 
 #define SERIALIZER_VERSION 1
 
+// Serializer holds the information needed to serialize data into file in binary format.
+// To minimize disk access, a memory buffer is used to store temporary result. When full, 
+// the buffer is dumped into file and cleared.
 struct Serializer {
     FILE* file = nullptr;
     int version = 0;
@@ -13,6 +16,17 @@ struct Serializer {
     size_t buffer_capacity = 0;
     size_t buffer_count = 0;
 };
+
+
+void write(Serializer& srl, void* data, size_t size);
+void read(Serializer& srl, void* data, size_t size);
+
+// Serialize type or struct with no allocated resource
+template <typename Type>
+void serialize(Serializer& srl, Type& data, bool save) {
+    if(save) write(srl, &data, sizeof(Type));
+    else      read(srl, &data, sizeof(Type));
+}
 
 void _serialize_error(const Serializer& s, const std::string& message, const char* file, int line) {
     printf("\n*** SERIALIZER ERROR ***\n");
@@ -25,6 +39,35 @@ void _serialize_error(const Serializer& s, const std::string& message, const cha
 }
 
 #define serialize_error(s,m) _serialize_error(s,m, __FILE__, __LINE__)
+
+Serializer make_serializer(const std::string& filename, bool save, size_t buffer_capacity = 0) {
+    Serializer srl;
+    srl.file = fopen(filename.c_str(), save? "w" : "r");
+    if (not srl.file) {
+        if(save) serialize_error(srl, "could not save data into " + filename);
+        else     serialize_error(srl, "could not load data from " + filename);
+    }
+
+    srl.buffer = (unsigned char*) malloc(buffer_capacity);
+    if(not srl.buffer) serialize_error(srl, "could not allocate buffer for " + filename);
+    srl.buffer_capacity = buffer_capacity;
+    fread(srl.buffer, buffer_capacity, 1, srl.file);
+
+    srl.version = SERIALIZER_VERSION;
+    serialize(srl, srl.version, save);
+    return srl;
+}
+
+
+void close_serializer(Serializer& srl) {
+    if(srl.buffer) {
+        fwrite(srl.buffer, srl.buffer_count, sizeof(unsigned char), srl.file);
+        free(srl.buffer);
+    }
+    if(srl.file) fclose(srl.file);
+    srl.buffer_capacity = 0;
+    srl.buffer_count = 0;
+}
 
 
 void write(Serializer& srl, void* data, size_t size) {
@@ -39,7 +82,7 @@ void write(Serializer& srl, void* data, size_t size) {
         return;
     }
     
-    // If buffer capacity is not enough, dump buffer to file.
+    // If buffer capacity is not enough, dump buffer to file and clear it.
     if(srl.buffer_count + size > srl.buffer_capacity) {
         fwrite(srl.buffer, srl.buffer_count * sizeof(unsigned char), 1, srl.file);
         srl.buffer_count = 0;
@@ -78,42 +121,6 @@ void read(Serializer& srl, void* data, size_t size) {
     }
 }
 
-// Serialize type or struct with no allocated resource
-template <typename Type>
-void serialize(Serializer& srl, Type& data, bool save) {
-    if(save) write(srl, &data, sizeof(Type));
-    else      read(srl, &data, sizeof(Type));
-}
-
-
-Serializer make_serializer(const std::string& filename, bool save, size_t buffer_capacity = 0) {
-    Serializer srl;
-    srl.file = fopen(filename.c_str(), save? "w" : "r");
-    if (not srl.file) {
-        if(save) serialize_error(srl, "could not save data into " + filename);
-        else     serialize_error(srl, "could not load data from " + filename);
-    }
-
-    srl.buffer = (unsigned char*) malloc(buffer_capacity);
-    if(not srl.buffer) serialize_error(srl, "could not allocate buffer for " + filename);
-    srl.buffer_capacity = buffer_capacity;
-    fread(srl.buffer, buffer_capacity, 1, srl.file);
-
-    srl.version = SERIALIZER_VERSION;
-    serialize(srl, srl.version, save);
-    return srl;
-}
-
-
-void close_serializer(Serializer& srl) {
-    if(srl.buffer) {
-        fwrite(srl.buffer, srl.buffer_count, sizeof(unsigned char), srl.file);
-        free(srl.buffer);
-    }
-    if(srl.file) fclose(srl.file);
-    srl.buffer_capacity = 0;
-    srl.buffer_count = 0;
-}
 
 // Serialize std::vector
 template <typename Type>
@@ -128,6 +135,24 @@ void serialize_vector(Serializer& srl, std::vector<Type>& vec, bool save) {
         read(srl, &count, sizeof(size_t));
         vec = std::vector<Type>(count);
         read(srl, vec.data(), sizeof(Type) * count);
+    }
+}
+
+// Serialize std::vector of structs with custom serialize function
+template <typename Type>
+void serialize_vector(Serializer& srl, std::vector<Type>& vec, bool save, std::function<void(Serializer&, Type&, bool)> serialize_obj) {
+    size_t count;
+    if(save) {
+        count = vec.size();
+        write(srl, &count, sizeof(size_t));
+        for (int i = 0; i < count; ++i)
+            serialize_obj(srl, vec[i], save);
+    }
+    else {
+        read(srl, &count, sizeof(size_t));
+        vec = std::vector<Type>(count);
+        for (int i = 0; i < count; ++i)
+            serialize_obj(srl, vec[i], save);
     }
 }
 
--- test.cpp
+++ test.cpp
@@ -2,16 +2,17 @@
 
 struct Prova {
     std::string name;
+    std::vector<int> vec;
     float val;
     int i;
 };
 
-void serialize(Serializer& srl, Prova& var, bool save) {
+void serialize_prova(Serializer& srl, Prova& var, bool save) {
     serialize_string(srl, var.name, save);
+    serialize_vector(srl, var.vec, save);
     serialize(srl, var.val, save);
     serialize(srl, var.i, save);
 }
-
 
 static bool operator==(const Prova& a, const Prova& b) {
     return a.val == b.val and a.i == b.i;
@@ -23,20 +24,33 @@ void print(Prova p) {
     printf("i: %d\n", p.i);
 }
 
+template <typename Type>
+void write_test(Type& x, const std::string& filename, size_t buffer_capacity) {
+    bool save = true;
+    auto writer = make_serializer("test.bin", save, 3);
+    serialize_vector<Prova>(writer, x, save, serialize_prova);
+    close_serializer(writer);   
+}
+
+template <typename Type>
+void read_test(Type& x, const std::string& filename, size_t buffer_capacity) {
+    bool save = false;
+    auto reader = make_serializer(filename, save, buffer_capacity);
+    Type y;
+    serialize_vector<Prova>(reader, y, save, serialize_prova);
+    assert(x == y);
+    close_serializer(reader);
+}
+
+
 int main() {
     bool save = true;
-    auto x0 = std::vector<Prova>{ Prova{"ciao", 10.0, 1}, Prova{"bello", 20.0, 2} };
-    auto writer = make_serializer("test.bin", save, 3);
-    serialize_vector(writer, x0, save);
-    close_serializer(writer);
+    auto filename = "test.bin";
+    auto x0 = std::vector<Prova>{ Prova{"ciao",{1,2,3,4}, 10.0, 1}, Prova{"bello", {7,7,7}, 20.0, 2} };
+    write_test(x0, filename, 3);
+    
+    for (int i = 0; i < 100; ++i)
+        read_test(x0, filename, i);
 
-    save = false;
-    for(int i = 0; i<30; i++) {
-        auto reader = make_serializer("test.bin", save, i);
-        std::vector<Prova> y0;
-        serialize_vector(reader, y0, save);
-        assert(x0 == y0);
-        close_serializer(reader);
-    }
     printf("Test successful!\n");
 }
